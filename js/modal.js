5751249abb10d5aeb0c20f1040a3c94f48d4f715
YuliaMoore <115689321+YuliaMoore@users.noreply.github.com>
2025-12-05 15:29:24 +0200

    css

--- js/modal.js
+++ js/modal.js
@@ -7,13 +7,3 @@ const toggleModal = () => modal.classList.toggle("is-hidden");
 modalBtnOpen.addEventListener("click", toggleModal);
 modalBtnClose.addEventListener("click", toggleModal);
 
-const text = document.getElementById("circle-text").innerText;
-const container = document.querySelector(".circular");
-container.innerHTML = "";
-
-[...text].forEach((char, i) => {
-  const span = document.createElement("span");
-  span.innerText = char;
-  span.style.transform = `rotate(${i * 8}deg)`; // Кут між символами
-  container.appendChild(span);
-});
